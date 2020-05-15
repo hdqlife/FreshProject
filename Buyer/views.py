625c9b9330c19c34aa26fdf87f3093e3bc4a5a98
hdq_life <hdqlife@163.com>
2020-05-15 13:57:27 +0800

    change Buyer/view.py

--- Buyer/views.py
+++ Buyer/views.py
@@ -9,7 +9,6 @@ from Qshop.views import setPassword,loginValid_buyer
 def index(request):
     types = Type.objects.filter(parent=0)
     result = []
-    # print("python java1")
     for t in types:
         d = {}
         d["type"] = t#每一种类型
